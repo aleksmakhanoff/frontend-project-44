f58a106960ee3497c197cc059fd7d4660734eb8e
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-04-13 22:14:56 +0700

    add brain-progression and task 8 done

---
+++ src/games/brain-progression.js
@@ -0,0 +1,28 @@
+import { getRandomNumber, playGame } from '../index.js';
+
+const isProgression = (start, step, length) => {
+  const result = [];
+  for (let i = 0; i <= length; i += 1) {
+    result.push(start + step * i);
+  }
+  return result;
+};
+
+const rule = 'What number is missing in the progression?';
+
+const generation = () => {
+  const numStart = getRandomNumber(1, 5);
+  const numStep = getRandomNumber(1, 5);
+  const numLength = getRandomNumber(5, 10);
+  const progression = isProgression(numStart, numStep, numLength);
+  const hidIndex = getRandomNumber(1, progression.length - 1);
+  const hidNumber = progression[hidIndex];
+  progression[hidIndex] = '..';
+  const question = progression.join(' ');
+  const correctAnswer = String(hidNumber);
+  return [question, correctAnswer];
+};
+
+const playProgressionGame = () => playGame(rule, generation);
+
+export default playProgressionGame;

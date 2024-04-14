4150893290da81c0583279122525af580c09b238
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-04-14 16:24:29 +0700

    add brain-prime and task 9 done

---
+++ src/games/brain-prime.js
@@ -0,0 +1,26 @@
+import { getRandomNumber, playGame } from '../index.js';
+
+const isSimple = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const generation = () => {
+  const randomNumber = getRandomNumber(1, 100);
+  const question = randomNumber.toString();
+  const correctAnswer = isSimple(randomNumber) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
+const playPrimeGame = () => playGame(rule, generation);
+
+export default playPrimeGame;

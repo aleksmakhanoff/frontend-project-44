1a06f49bea74067c29425f1d68b5dfcbab9ebb67
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-04-18 11:51:54 +0700

    added const roundCount and renamed generation

--- src/index.js
+++ src/index.js
@@ -2,14 +2,16 @@ import readlineSync from 'readline-sync';
 
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
-export const playGame = (rule, generation) => {
+export const playGame = (rule, gameBasisGeneration) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${name}!`);
   console.log(rule);
 
-  for (let i = 0; i < 3; i += 1) {
-    const [question, correctAnswer] = generation();
+  const roundCount = 3;
+
+  for (let i = 0; i < roundCount; i += 1) {
+    const [question, correctAnswer] = gameBasisGeneration();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
     if (userAnswer === correctAnswer) {

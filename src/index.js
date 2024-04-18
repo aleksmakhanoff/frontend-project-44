023d7ab1e70ec0604b0484399d69e265b2a3f628
aleksmakhanoff <152077911+aleksmakhanoff@users.noreply.github.com>
2024-04-18 13:37:22 +0700

    made oneNumberGeneration

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import readlineSync from 'readline-sync';
 
 export const getRandomNumber = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
 
+export const oneNumberGeneration = (checkRule) => {
+  const randomNumber = getRandomNumber(1, 100);
+  const question = randomNumber.toString();
+  const correctAnswer = checkRule(randomNumber) ? 'yes' : 'no';
+  return [question, correctAnswer];
+};
+
 export const playGame = (rule, gameBasisGeneration) => {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name? ');
@@ -14,13 +21,13 @@ export const playGame = (rule, gameBasisGeneration) => {
     const [question, correctAnswer] = gameBasisGeneration();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
+    if (userAnswer !== correctAnswer) {
       console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
       console.log(`Let's try again, ${name}!`);
+      console.log('Correct!');
       return;
     }
+    console.log('Correct!');
   }
   console.log(`Congratulations, ${name}!`);
 };
